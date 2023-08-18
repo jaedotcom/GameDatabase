176ae55df3a141f2348e75b3333dd98d30db7ece
Jae Kim <83146793+jaedotcom@users.noreply.github.com>
2023-08-19 09:19:10 +1200

    trying to add list of Games from the CSV file on Home.html - It's not showing. Wondering if I should revisit my A1 and see how I used the CSV Utility.

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,24 @@
 """App entry point."""
 from games import create_app
 from flask import Flask, render_template
+import csv
+
 app = create_app()
 
 @app.route('/')
 def show_home():
-    return render_template('home.html')
-
+    games = []
+    with open('games.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            games.append(row)
+    return render_template('home.html', games=games)
 
 @app.route('/gameDescription')
 def show_gameDescription():
     return render_template('gameDescription.html')
+
+
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5000, threaded=False)

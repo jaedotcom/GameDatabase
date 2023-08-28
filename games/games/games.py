57cc70baf75fe4dc13335332f74b0852fa248651
Jae Kim <83146793+jaedotcom@users.noreply.github.com>
2023-08-29 11:04:25 +1200

    Genre sidebar now works on Games description page

--- games/games/games.py
+++ games/games/games.py
@@ -1,6 +1,7 @@
 
 from flask import Blueprint, render_template, request
 from games.games import services
+from games.home import services as sv
 import games.adapters.repository as repo
 
 games_bp = Blueprint('games_bp', __name__)
@@ -18,8 +19,8 @@ def games():
     start_idx = (page - 1) * games_per_page
     end_idx = start_idx + games_per_page
     current_games = all_games[start_idx:end_idx]
-
-    return render_template('games.html', some_game=current_games, current_page=page, num_pages=total_pages)
+    all_genres = sv.get_genres(repo.repo_instance)
+    return render_template('games.html', some_game=current_games, current_page=page, num_pages=total_pages, all_genres=all_genres)
 
 
 

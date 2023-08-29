e138ee60796988c9b797226bd5380eb58e694b92
Jae Kim <83146793+jaedotcom@users.noreply.github.com>
2023-08-29 16:04:08 +1200

    need to do some work on genreBar.html

--- games/genreBar/genreBar.py
+++ games/genreBar/genreBar.py
@@ -2,11 +2,12 @@ from flask import Blueprint, render_template, request
 from games.home import services
 from .services import get_games_by_genre
 import games.adapters.repository as repo
-
+from .. import MemoryRepository
 
 genreBar_blueprint = Blueprint(
     'genreBar_bp', __name__)
 
+repo = MemoryRepository()
 
 @genreBar_blueprint.route('/genreBar', methods=['GET'])
 def genreBar():
@@ -19,3 +20,8 @@ def genreBar():
         genre_games = []
 
     return render_template('genreBar.html', all_genres=all_genres, genre_games=genre_games)
+
+@genreBar_blueprint.route('/genreBar/<genre>')
+def genre_bar(genre):
+    genre_filtered_games = repo.get_games_by_genre(genre)
+    return render_template('gameGenre.html', games=genre_filtered_games)

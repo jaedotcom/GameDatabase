51ea5024b5f29b50bdb5535c400b6c7b38a4b108
Leisha <leishasteed@gmail.com>
2023-10-02 19:57:32 +1300

    starting task 1 ...
    attempting to start task1

--- games/__init__.py
+++ games/__init__.py
@@ -10,12 +10,47 @@ import games.adapters.repository as repo
 from games.adapters.memoryRepository import populate
 from games.adapters.memoryRepository import MemoryRepository
 
+from games.adapters.database_repository import SqlAlchemyRepository
+from games.adapters.orm import metadata, map_model_to_tables
+
+# imports from SQLAlchemy
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.orm import sessionmaker, clear_mappers
+from sqlalchemy.pool import NullPool
+
 
 def create_app(test_config=None):
     """Construct the core application."""
 
     # Create the Flask app object.
     app = Flask(__name__)
+
+    database_uri = 'sqlite:///games.db'
+    database_echo = app.config['SQLALCHEMY_ECHO']
+    database_engine = create_engine(database_uri, connect_args={"check_same_thread": False}, poolclass=NullPool,
+                                    echo=database_echo)
+    session_factory = sessionmaker(autocommit=False, autoflush=True, bind=database_engine)
+    repo.repo_instance = database_repository.SqlAlchemyRepository(session_factory)
+
+    if app.config['TESTING'] == 'True' or len(database_engine.table_names()) == 0:
+        print("REPOPULATING DATABASE...")
+        # For testing, or first-time use of the web application, reinitialise the database.
+        clear_mappers()
+        metadata.create_all(database_engine)  # Conditionally create database tables.
+        for table in reversed(metadata.sorted_tables):  # Remove any data from the tables.
+            database_engine.execute(table.delete())
+
+        # Generate mappings that map domain model classes to the database tables.
+        map_model_to_tables()
+
+        database_mode = True
+        repository_populate.populate(data_path, repo.repo_instance, database_mode)
+        print("REPOPULATING DATABASE... FINISHED")
+
+    else:
+        # Solely generate mappings that map domain model classes to the database tables.
+        map_model_to_tables()
+
     app.config.from_object('config.Config')
     data_path = Path('games') / 'adapters' / 'data'
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
@@ -54,6 +89,19 @@ def create_app(test_config=None):
 
         from .reviews import reviews
         app.register_blueprint(reviews.reviews_blueprint)
+
+        # Register a callback the makes sure that database sessions are associated with http requests
+        # We reset the session inside the database repository before a new flask request is generated
+        @app.before_request
+        def before_flask_http_request_function():
+            if isinstance(repo.repo_instance, database_repository.SqlAlchemyRepository):
+                repo.repo_instance.reset_session()
+
+        # Register a tear-down method that will be called after each request has been processed.
+        @app.teardown_appcontext
+        def shutdown_session(exception=None):
+            if isinstance(repo.repo_instance, database_repository.SqlAlchemyRepository):
+                repo.repo_instance.close_session()
 
 
     return app
---
+++ games/adapters/datareader/orm.py
@@ -0,0 +1,55 @@
+from sqlalchemy import (
+    Table, MetaData, Column, Integer, String, Text, Float, ForeignKey
+)
+from sqlalchemy.orm import mapper, relationship
+
+from games.domainmodel.model import Game, Publisher, Genre
+
+# global variable giving access to the MetaData (schema) information of the database
+metadata = MetaData()
+
+publishers_table = Table(
+    'publishers', metadata,
+    # We only want to maintain those attributes that are in our domain model
+    # For publisher, we only have name.
+    Column('name', String(255), primary_key=True)  # nullable=False, unique=True)
+)
+
+games_table = Table(
+    'games', metadata,
+    Column('game_id', Integer, primary_key=True),
+    Column('game_title', Text, nullable=False),
+    Column('game_price', Float, nullable=False),
+    Column('release_date', String(50), nullable=False),
+    Column('game_description', String(255), nullable=True),
+    Column('game_image_url', String(255), nullable=True),
+    Column('game_website_url', String(255), nullable=True),
+    Column('publisher_name', ForeignKey('publishers.name'))
+)
+
+genres_table = Table(
+    'genres', metadata,
+    # For genre again we only have name.
+    Column('genre_name', String(64), primary_key=True, nullable=False)
+)
+
+
+def map_model_to_tables():
+    mapper(Publisher, publishers_table, properties={
+        '_Publisher__publisher_name': publishers_table.c.name,
+    })
+
+    mapper(Game, games_table, properties={
+        '_Game__game_id': games_table.c.game_id,
+        '_Game__game_title': games_table.c.game_title,
+        '_Game__price': games_table.c.game_price,
+        '_Game__release_date': games_table.c.release_date,
+        '_Game__description': games_table.c.game_description,
+        '_Game__image_url': games_table.c.game_image_url,
+        '_Game__website_url': games_table.c.game_website_url,
+        '_Game__publisher': relationship(Publisher)
+    })
+
+    mapper(Genre, genres_table, properties={
+        '_Genre__genre_name': genres_table.c.genre_name,
+    })

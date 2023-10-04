2ff76e4a00c01c17023c2882926c66c00a7846ea
Jae Kim <83146793+jaedotcom@users.noreply.github.com>
2023-10-04 19:42:54 +1300

    repo populate

--- games/adapters/datareader/repository_populate.py
+++ games/adapters/datareader/repository_populate.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 
 from games.adapters.repository import AbstractRepository
-#from games.adapters.csv_data_importer import load_comments, load_users, load_articles_and_tags
+#from games.adapters.csv_data_importer import load_users, load_games, load_reviews
 
 
 def populate(data_path: Path, repo: AbstractRepository, database_mode: bool):
+    # load_users(data_path, repo)
+    # games = load_games(data_path, repo)
+    # load_reviews(reviews, data_path, repo)
+
     pass

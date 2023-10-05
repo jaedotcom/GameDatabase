74adc193cceb21c4ea25ec85111242db566b33db
Leisha <leishasteed@gmail.com>
2023-10-05 16:31:38 +1300

    update __init__.py
    it's in a place where it might work if we get everything else set up as well

--- games/__init__.py
+++ games/__init__.py
@@ -49,28 +49,14 @@ def create_app(test_config=None):
         data_path = app.config[
             'TEST_DATA_PATH']  # Make sure to change TEST_DATA_PATH : games/tests/data and TESTING: True in .env for testing
 
-
-    if app.config['TESTING'] == 'True':
-        print("REPOPULATING DATABASE...")
-        # For testing, or first-time use of the web application, reinitialise the database.
-        clear_mappers()
-        metadata.create_all(database_engine)  # Conditionally create database tables.
-        for table in reversed(metadata.sorted_tables):  # Remove any data from the tables.
-            database_engine.execute(table.delete())
-
-        # Generate mappings that map domain model classes to the database tables.
-        map_model_to_tables()
-
-        database_mode = True
-        repository_populate.populate(data_path, repo.repo_instance, database_mode)
-        print("REPOPULATING DATABASE... FINISHED")
-
-    elif app.config['REPOSITORY'] == 'memory':
+    if app.config['REPOSITORY'] == 'memory':
         # Create the MemoryRepository implementation for a memory-based repository.
         repo.repo_instance = memoryRepository.MemoryRepository()
         # fill the content of the repository from the provided csv files (has to be done every time we start app!)
         database_mode = False
         repository_populate.populate(data_path, repo.repo_instance, database_mode)
+
+
     elif app.config['REPOSITORY'] == 'database':
         database_uri = app.config['SQLALCHEMY_DATABASE_URI']
         database_echo = app.config['SQLALCHEMY_ECHO']
@@ -78,18 +64,33 @@ def create_app(test_config=None):
                                         echo=database_echo)
         session_factory = sessionmaker(autocommit=False, autoflush=True, bind=database_engine)
         repo.repo_instance = database_repository.SqlAlchemyRepository(session_factory)
+        if app.config['TESTING'] == 'True' or len(database_engine.table_names()) == 0:
+            print("REPOPULATING DATABASE...")
+            # For testing, or first-time use of the web application, reinitialise the database.
+            clear_mappers()
+            metadata.create_all(database_engine)  # Conditionally create database tables.
+            for table in reversed(metadata.sorted_tables):  # Remove any data from the tables.
+                database_engine.execute(table.delete())
+
+            # Generate mappings that map domain model classes to the database tables.
+            map_model_to_tables()
+
+            database_mode = True
+            repository_populate.populate(data_path, repo.repo_instance, database_mode)
+            print("REPOPULATING DATABASE... FINISHED")
+
+
+
 
     else:
         # Solely generate mappings that map domain model classes to the database tables.
         map_model_to_tables()
 
-    app.config.from_object('config.Config')
-    data_path = Path('games') / 'adapters' / 'data'
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 
-    repo.repo_instance = MemoryRepository()
-    populate(data_path, repo.repo_instance)
+    #
+    # repo.repo_instance = MemoryRepository()
+    # populate(data_path, repo.repo_instance)
 
     # Build the application - these steps require an application context.
     with app.app_context():

e8752f816e8d468e0ee4e81747bdf48f8bd54437
Jae Kim <83146793+jaedotcom@users.noreply.github.com>
2023-10-10 14:28:52 +1300

    Search bar simplified. searches dynamically. amazing

--- games/home/home.py
+++ games/home/home.py
@@ -16,27 +16,45 @@ def home():
 
 @home_blueprint.route('/search', methods=('GET', 'POST'))
 def search_results():
-    global genre_filter
     if request.method == 'POST':
         search = request.form['search'].strip()
-        genre_filter = request.form['genre'].strip()
     else:
         search = 'failure'
     all_games = get_games(repo.repo_instance)
-    game_titles = []
-    all_genres = set()
-    for game in all_games:
-        for g in game['genres']:
-            all_genres.add(g.lower())
-    all_genres = list(set(all_genres))
-    for game in all_games:
-        if game['title'] not in game_titles:
-            game_titles.append(game['title'].lower())
     found_games = []
     for game in all_games:
-        list_of_genres = game['genres']
-        if (search.lower() in game_titles) and (genre_filter.lower() in all_genres):
-            if game['title'].lower() == search.lower():
-                if genre_filter in list_of_genres:
-                    found_games.append(game)
-    return render_template('search/gameSearbarResult.html', search_query=search, games=found_games, all_genres=all_genres)
+        if search.lower() in game['title'].lower():
+            found_games.append(game)
+    print(found_games)
+
+
+    return render_template('search/gameSearchBarResult.html', search_query=search, games=found_games)
+
+
+#BACK UP COPY!
+# @home_blueprint.route('/search', methods=('GET', 'POST'))
+# def search_results():
+#     global genre_filter
+#     if request.method == 'POST':
+#         search = request.form['search'].strip()
+#         genre_filter = request.form['genre'].strip()
+#     else:
+#         search = 'failure'
+#     all_games = get_games(repo.repo_instance)
+#     game_titles = []
+#     all_genres = set()
+#     for game in all_games:
+#         for g in game['genres']:
+#             all_genres.add(g.lower())
+#     all_genres = list(set(all_genres))
+#     for game in all_games:
+#         if game['title'] not in game_titles:
+#             game_titles.append(game['title'].lower())
+#     found_games = []
+#     for game in all_games:
+#         list_of_genres = game['genres']
+#         if (search.lower() in game_titles) and (genre_filter.lower() in all_genres):
+#             if game['title'].lower() == search.lower():
+#                 if genre_filter in list_of_genres:
+#                     found_games.append(game)
+#     return render_template('search/gameSearchBarResult.html', search_query=search, games=found_games, all_genres=all_genres)
